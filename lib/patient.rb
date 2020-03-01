364d7b516eec6d90583eb90ecaa4b513b98aef9e
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-03-01 19:14:07 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Patient
 
   attr_accessor :name, :doctor
@@ -13,6 +14,7 @@ class Patient
   end
 
   def appointments
+  binding.pry
     Appointment.all.select {|appointment| appointment.patient == self}
   end
 
